c9187ef6b6087c3795b21745b4eee93d79b48604
avearmin <arminradoncic1@gmail.com>
2023-09-06 00:48:51 -0400

    Add basic REPL implementation
    I have implemented a basic REPL supporting an exit and help command.
    Additionally, I wrote the help command to dynamically generate the help
    message to ensure I don't need to update the commandHelp() function to
    account for the new commands as I add more commands.
    
    Overall, this commit sets the stage for expanding the REPL.

--- main.go
+++ main.go
@@ -1,9 +1,65 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func CommandMap() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandHelp() error {
+	fmt.Print(
+		"Welcome to the Pokedex!\n",
+		"Usage:\n\n",
+	)
+	cmdMap := CommandMap()
+	for key, value := range cmdMap {
+		fmt.Printf("%s: %s\n", key, value.description)
+	}
+	fmt.Println()
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	scanner := bufio.NewScanner(os.Stdin)
+	cmdMap := CommandMap()
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		userInput := scanner.Text()
+		cmd, ok := cmdMap[userInput]
+		if !ok {
+			fmt.Printf("%s is not a valid command", userInput)
+			continue
+		}
+		err := cmd.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
